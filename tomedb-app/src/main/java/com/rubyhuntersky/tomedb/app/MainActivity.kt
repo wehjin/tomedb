6d2741f6c3a70e18d4a2889d9f3c4b8afde26d73
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2019-02-24 19:08:52 -0800

    Setup gradle

---
+++ tomedb-app/src/main/java/com/rubyhuntersky/tomedb/app/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.rubyhuntersky.tomedb.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rubyhuntersky.tomedb.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ tomedb/src/main/java/com/rubyhuntersky/tomedb/TomeDb.kt
@@ -0,0 +1,3 @@
+package com.rubyhuntersky.tomedb
+
+class TomeDb
