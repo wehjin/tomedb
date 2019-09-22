bf0c740774a12368e2eda12841ac78e79c11f15a
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2019-09-22 10:37:12 -0700

    Convert Scheme to sealed class.

--- tomedb/src/main/java/com/rubyhuntersky/tomedb/attributes/Scheme.kt
+++ tomedb/src/main/java/com/rubyhuntersky/tomedb/attributes/Scheme.kt
@@ -2,35 +2,47 @@ package com.rubyhuntersky.tomedb.attributes
 
 import com.rubyhuntersky.tomedb.basics.Keyword
 
-enum class Scheme : Attribute {
-
-    NAME {
-        override val valueType = ValueType.KEYWORD
-        override val cardinality = Cardinality.ONE
-        override val description = "The unique name of an attribute."
-    },
-    VALUETYPE {
-        override val valueType = ValueType.KEYWORD
-        override val cardinality = Cardinality.ONE
-        override val description = "The type of the value that can be associated with a value."
-    },
-    CARDINALITY {
-        override val valueType = ValueType.KEYWORD
-        override val cardinality = Cardinality.ONE
-        override val description =
-            "Specifies whether an attribute associates an entity with a single value or a set of values."
-    },
-    DESCRIPTION {
-        override val valueType = ValueType.STRING
-        override val cardinality = Cardinality.ONE
-        override val description = "Specifies a documentation string"
-    };
+sealed class Scheme : Attribute {
 
     override fun toString(): String = attrName.toString()
 
     companion object {
-        val cardinalities: Map<Keyword, Cardinality> =
-            values().associateBy(Scheme::attrName, Scheme::cardinality)
+        private val attrs by lazy {
+            listOf(NAME, VALUETYPE, CARDINALITY, DESCRIPTION)
+        }
+
+        val cardinalities: Map<Keyword, Cardinality> by lazy {
+            attrs.associateBy(Scheme::attrName, Scheme::cardinality)
+        }
+    }
+
+    object NAME : Scheme() {
+        override val valueType = ValueType.KEYWORD
+        override val cardinality = Cardinality.ONE
+        override val description = "The unique name of an attribute."
+
+    }
+
+    object VALUETYPE : Scheme() {
+        override val valueType = ValueType.KEYWORD
+        override val cardinality = Cardinality.ONE
+        override val description = "The type of the value that can be associated with a value."
+
+    }
+
+    object CARDINALITY : Scheme() {
+        override val valueType = ValueType.KEYWORD
+        override val cardinality = Cardinality.ONE
+        override val description =
+            "Specifies whether an attribute associates an entity with a single value or a set of values."
+
+    }
+
+    object DESCRIPTION : Scheme() {
+        override val valueType = ValueType.STRING
+        override val cardinality = Cardinality.ONE
+        override val description = "Specifies a documentation string"
+
     }
 }
 
