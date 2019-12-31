4a66c2cad19933187005c27818c8de5aca06c788
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2019-12-30 18:16:12 -0800

    Remove deprecated of functions from Ent.

--- tomedb/src/main/java/com/rubyhuntersky/tomedb/basics/Ent.kt
+++ tomedb/src/main/java/com/rubyhuntersky/tomedb/basics/Ent.kt
@@ -1,9 +1,5 @@
 package com.rubyhuntersky.tomedb.basics
 
-import com.rubyhuntersky.tomedb.attributes.Attribute
-import com.rubyhuntersky.tomedb.attributes.AttributeGroup
-import com.rubyhuntersky.tomedb.attributes.attrName
-import com.rubyhuntersky.tomedb.attributes.groupName
 import kotlin.math.absoluteValue
 
 data class Ent(val number: Long) {
@@ -12,22 +8,4 @@ data class Ent(val number: Long) {
     }
 
     fun mix(ent: Ent): Ent = Ent((31 * this.number + ent.number).absoluteValue)
-
-    companion object {
-
-        @Deprecated("No replacement")
-        fun of(group: AttributeGroup, index: Long): Ent {
-            return Ident.Local(group.groupName, index).toEnt()
-        }
-
-        @Deprecated("No replacement")
-        fun <T : Any> of(attr: Attribute<*>, value: T): Ent {
-            return of(attr.attrName, value)
-        }
-
-        @Deprecated("No replacement")
-        fun <T : Any> of(attr: Keyword, value: T): Ent {
-            return Ent(attr.keywordGroup.hashCode().let { 31 * it + value.hashCode() }.toLong().absoluteValue)
-        }
-    }
 }
